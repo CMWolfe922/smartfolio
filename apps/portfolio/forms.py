deaef36ce302ab0d668cf31ee8fb6ef1fff36da0
Charles Wolfe <cmwolfe.dev@gmail.com>
2022-07-05 12:31:24 -0500

    Created new forms and updated the models to create a NewUser model that replaces the Django base User model

--- apps/portfolio/forms.py
+++ apps/portfolio/forms.py
@@ -8,6 +8,15 @@ class UserForm(forms.ModelForm):
 # To create a new user import the UserCreationForm from django.contrib.auth.forms
 class RegisterForm(UserCreationForm):
     # Add any additional fields that I want added to the new user form
+    email = forms.EmailField(required=True, unique=True, max_length=255)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserProgileForm(forms.ModelForm):
+    # add the fields that need to be filled in for the user to be able to
+    # have a profile and so that user's profile is searchable.
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
     personal_email = forms.EmailField()
@@ -18,7 +27,6 @@ class RegisterForm(UserCreationForm):
     work_experience = forms.TextField(max_length=1000)
     skills = forms.ModelsChoiceField()
 
-
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fiels = ['first_name', 'last_name', 'personal_email', 'work_email', 'cell_phone', 'work_phone', 'bio', 'work_experience', 'skills']
